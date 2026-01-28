d1449dc3798ab55a9da39af05b25f02efbf207d3
iamwotjr99 <iamwotjr@gmail.com>
2026-01-28 17:29:29 +0900

    #74 - [FEAT] UTIL : JWT 상태에 따른 커스텀 예외 처리 구현

--- apps/server-auth/src/main/java/com/assetmind/server_auth/global/error/ErrorCode.java
+++ apps/server-auth/src/main/java/com/assetmind/server_auth/global/error/ErrorCode.java
@@ -20,7 +20,12 @@ public enum ErrorCode {
     INVALID_VERIFICATION_CODE(HttpStatus.BAD_REQUEST, "U004", "유효하지 않은 인증 코드입니다."),
 
     // Jwt 토큰 에러
-    INVALID_SIGN_UP_TOKEN(HttpStatus.BAD_REQUEST, "U003", "유효하지 않거나 용도가 잘못된 회원가입 토큰입니다.");
+    INVALID_SIGN_UP_TOKEN(HttpStatus.BAD_REQUEST, "T001", "유효하지 않거나 용도가 잘못된 회원가입 토큰입니다."),
+    EXPIRED_TOKEN(HttpStatus.UNAUTHORIZED, "T003", "토큰이 만료되었습니다."),
+    INVALID_TOKEN_SIGNATURE(HttpStatus.UNAUTHORIZED, "T004", "토큰 서명이 유효하지 않습니다."),
+    INVALID_TOKEN_TYPE(HttpStatus.UNAUTHORIZED, "T004", "잘못된 토큰 타입입니다."),
+    INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "T005", "유효하지 않은 토큰입니다."),
+    INVALID_REFRESH_TOKEN(HttpStatus.UNAUTHORIZED, "T006", "유효하지 않거나 만료된 리프레시 토큰입니다.");
 
     private final HttpStatus status;
     private final String code;
--- apps/server-auth/src/main/java/com/assetmind/server_auth/user/application/provider/AuthTokenProvider.java
+++ apps/server-auth/src/main/java/com/assetmind/server_auth/user/application/provider/AuthTokenProvider.java
@@ -1,9 +1,14 @@
 package com.assetmind.server_auth.user.application.provider;
 
 import com.assetmind.server_auth.global.common.JwtProcessor;
+import com.assetmind.server_auth.global.error.ErrorCode;
 import com.assetmind.server_auth.user.application.dto.TokenSetDto;
 import com.assetmind.server_auth.user.domain.type.UserRole;
+import com.assetmind.server_auth.user.exception.AuthException;
 import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.MalformedJwtException;
+import java.security.SignatureException;
 import java.util.Map;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
@@ -50,7 +55,7 @@ public class AuthTokenProvider {
      * @return UUID
      */
     public UUID getUserIdFromToken(String token) {
-        Claims claims = jwtProcessor.parse(token);
+        Claims claims = getClaims(token);
 
         return UUID.fromString(claims.getSubject());
     }
@@ -61,9 +66,42 @@ public class AuthTokenProvider {
      * @return GUEST/USER
      */
     public UserRole getRoleFromToken(String token) {
-        Claims claims = jwtProcessor.parse(token);
+        Claims claims = getClaims(token);
         String roleString = claims.get(ROLE_CLAIM, String.class);
-        return UserRole.valueOf(roleString);
 
+        return UserRole.valueOf(roleString);
+    }
+
+    /**
+     * 토큰 값을 검증하고 예외를 던진다.
+     * @param token
+     */
+    public void validateToken(String token) {
+        getClaims(token);
+    }
+
+    private Claims getClaims(String token) {
+        try {
+            Claims claims = jwtProcessor.parse(token);
+
+            // 로그인 시 받은 Auth 용 토큰이 아니라면 타입 에러
+            if (claims.get(ROLE_CLAIM) == null) {
+                throw new AuthException(ErrorCode.INVALID_TOKEN_TYPE);
+            }
+
+            return claims;
+        } catch (ExpiredJwtException e) {
+            // 토큰이 만료 됐을 때
+            throw new AuthException(ErrorCode.EXPIRED_TOKEN);
+        } catch (MalformedJwtException e) {
+            // 토큰 서명이 유효하지 않을 떄
+            throw new AuthException(ErrorCode.INVALID_TOKEN_SIGNATURE);
+        } catch (AuthException e) {
+            // try 문에서 던진 예외가 다른 예외로 잡히지 않기 위함
+            throw e;
+        } catch (Exception e) {
+            // 그외 모든 오류
+            throw new AuthException(ErrorCode.INVALID_TOKEN);
+        }
     }
 }
