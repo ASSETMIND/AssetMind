d4b06e1960836f746a7ba47adb0bab3e00e75064
iamwotjr99 <iamwotjr@gmail.com>
2026-02-12 21:04:56 +0900

    #162 - [FEAT] UTIL : 이벤트 DTO를 Entity로 매핑 또는 역매핑 해주는 StockMapper 구현

---
+++ apps/server-stock/src/main/java/com/assetmind/server_stock/stock/application/mapper/StockMapper.java
@@ -0,0 +1,41 @@
+package com.assetmind.server_stock.stock.application.mapper;
+
+import com.assetmind.server_stock.stock.application.listener.dto.RealTimeStockTradeEvent;
+import com.assetmind.server_stock.stock.infrastructure.persistence.entity.StockDataEntity;
+import com.assetmind.server_stock.stock.infrastructure.persistence.entity.StockPriceRedisEntity;
+import org.springframework.stereotype.Component;
+
+/**
+ * 주가 데이터 이벤트 DTO를 저장소(JPA, Redis)에 쓰이는 Entity로 매핑한다. 또는
+ * 역으로 매핑한다.
+ *
+ * 1. event -> entity
+ */
+@Component
+public class StockMapper {
+
+    public StockPriceRedisEntity toRedisEntity(RealTimeStockTradeEvent event, String stockName) {
+        return StockPriceRedisEntity.builder()
+                .stockCode(event.stockCode())
+                .stockName(stockName)
+                .currentPrice(event.currentPrice())
+                .priceChange(event.priceChange())
+                .changeRate(event.changeRate())
+                .changeSign(event.changeSign())
+                .cumulativeAmount(event.cumulativeAmount())
+                .cumulativeVolume(event.cumulativeVolume())
+                .time(event.time())
+                .build();
+    }
+
+    public StockDataEntity toJpaEntity(RealTimeStockTradeEvent event, String stockName) {
+        return StockDataEntity.builder()
+                .stockCode(event.stockCode())
+                .price(event.currentPrice())
+                .time(event.time())
+                .volume(event.cumulativeVolume())
+                .changeRate(event.changeRate())
+                .createdAt(event.eventTimeStamp())
+                .build();
+    }
+}
