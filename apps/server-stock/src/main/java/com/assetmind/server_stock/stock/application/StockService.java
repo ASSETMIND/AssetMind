b753f6ba166dde15014e889ba5751805079e9997
iamwotjr99 <iamwotjr@gmail.com>
2026-02-13 16:25:19 +0900

    #162 - [FEAT] SERVICE : StockService에 커스텀 예외 추가

--- apps/server-stock/src/main/java/com/assetmind/server_stock/stock/application/StockService.java
+++ apps/server-stock/src/main/java/com/assetmind/server_stock/stock/application/StockService.java
@@ -1,10 +1,13 @@
 package com.assetmind.server_stock.stock.application;
 
+import com.assetmind.server_stock.global.error.ErrorCode;
 import com.assetmind.server_stock.stock.application.listener.dto.RealTimeStockTradeEvent;
 import com.assetmind.server_stock.stock.application.mapper.StockMapper;
 import com.assetmind.server_stock.stock.application.provider.StockMetadataProvider;
 import com.assetmind.server_stock.stock.domain.repository.StockHistoryRepository;
 import com.assetmind.server_stock.stock.domain.repository.StockSnapshotRepository;
+import com.assetmind.server_stock.stock.exception.InvalidStockParameterException;
+import com.assetmind.server_stock.stock.exception.StockNotFoundException;
 import com.assetmind.server_stock.stock.infrastructure.persistence.entity.StockDataEntity;
 import com.assetmind.server_stock.stock.infrastructure.persistence.entity.StockPriceRedisEntity;
 import java.util.List;
@@ -28,6 +31,11 @@ public class StockService {
     // 실시간 주가 처리
     @Transactional // 쓰기 작업
     public void processRealTimeTrade(RealTimeStockTradeEvent event) {
+
+        if (event == null || event.stockCode() == null || event.stockCode().isBlank()) {
+            throw new InvalidStockParameterException(ErrorCode.INVALID_STOCK_PARAMETER);
+        }
+
         // 캐싱된 국내 전체 주식에서 실시간 주식 데이터의 주식 이름 추출
         String stockName = stockMetadataProvider.getStockName(event.stockCode());
 
@@ -50,6 +58,10 @@ public class StockService {
 
     // 특정 주식의 시계열 데이터 조회
     public List<StockDataEntity> getStockRecentHistory(String stockCode, int limit) {
+        if (stockCode == null || stockCode.isBlank()) {
+            throw new InvalidStockParameterException(ErrorCode.INVALID_STOCK_PARAMETER);
+        }
+
         return stockHistoryRepository.findRecentData(stockCode, limit);
     }
 }
