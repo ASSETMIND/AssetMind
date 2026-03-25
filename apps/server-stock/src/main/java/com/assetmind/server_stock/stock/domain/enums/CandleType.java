2921872032318ba9b4212c1b3403294b8f81a179
iamwotjr99 <iamwotjr@gmail.com>
2026-03-25 15:00:10 +0900

    #285 - [FEAT] DTO : Candle Type Enum, OHLCV DTO 구현

---
+++ apps/server-stock/src/main/java/com/assetmind/server_stock/stock/domain/enums/CandleType.java
@@ -0,0 +1,26 @@
+package com.assetmind.server_stock.stock.domain.enums;
+
+import com.assetmind.server_stock.global.error.ErrorCode;
+import com.assetmind.server_stock.stock.exception.InvalidStockParameterException;
+import lombok.Getter;
+
+@Getter
+public enum CandleType {
+    MIN_1("1m"),
+    DAY_1("1d");
+
+    private final String value;
+
+    CandleType(String value) {
+        this.value = value;
+    }
+
+    public static CandleType from(String value) {
+        for (CandleType type : values()) {
+            if (type.getValue().equals(value)) {
+                return type;
+            }
+        }
+        throw new InvalidStockParameterException("지원하지 않는 캔들 타입입니다.:" + value, ErrorCode.INVALID_STOCK_PARAMETER);
+    }
+}
