14e9322c8f69782f924794994f3f130074ff17fc
iamwotjr99 <iamwotjr@gmail.com>
2026-05-03 13:53:02 +0900

    #359 - [FEAT] ENUM : CandleType이 맞지 않을 경우 예외를 던질 때 에러 코드를 변경

--- apps/server-stock/src/main/java/com/assetmind/server_stock/stock/domain/enums/CandleType.java
+++ apps/server-stock/src/main/java/com/assetmind/server_stock/stock/domain/enums/CandleType.java
@@ -1,6 +1,7 @@
 package com.assetmind.server_stock.stock.domain.enums;
 
 import com.assetmind.server_stock.global.error.ErrorCode;
+import com.assetmind.server_stock.stock.exception.InvalidChartParameterException;
 import com.assetmind.server_stock.stock.exception.InvalidStockParameterException;
 import lombok.Getter;
 
@@ -32,6 +33,6 @@ public enum CandleType {
                 return type;
             }
         }
-        throw new InvalidStockParameterException("지원하지 않는 캔들 타입입니다.:" + value, ErrorCode.INVALID_STOCK_PARAMETER);
+        throw new InvalidChartParameterException(ErrorCode.INVALID_CHART_PARAMETER, "지원하지 않는 캔들 타입입니다.:" + value);
     }
 }
