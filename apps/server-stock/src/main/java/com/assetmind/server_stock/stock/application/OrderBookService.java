a199d2f51e239f81f310d55dc3828cb6d9a5e9a3
iamwotjr99 <iamwotjr@gmail.com>
2026-05-21 14:44:31 +0900

    #401 - [FEAT] SERVICE : 특정 종목의 호가 단건 조회를 위한 OrderBookService 구현

---
+++ apps/server-stock/src/main/java/com/assetmind/server_stock/stock/application/OrderBookService.java
@@ -0,0 +1,22 @@
+package com.assetmind.server_stock.stock.application;
+
+import com.assetmind.server_stock.global.error.ErrorCode;
+import com.assetmind.server_stock.market_access.domain.OrderBook;
+import com.assetmind.server_stock.stock.exception.InvalidStockParameterException;
+import com.assetmind.server_stock.stock.infrastructure.throttling.OrderBookCacheRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class OrderBookService {
+
+    private final OrderBookCacheRepository cacheRepository;
+
+    /**
+     * 특정 종목의 최신 호가 스냅샷을 조회
+     */
+    public OrderBook getOrderBookSnapshot(String stockCode) {
+        return cacheRepository.getSnapshot(stockCode);
+    }
+}
