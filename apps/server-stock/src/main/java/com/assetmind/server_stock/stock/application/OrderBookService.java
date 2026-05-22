fdbc01d36f2732ad4b6407369440eef2210c92e6
iamwotjr99 <iamwotjr@gmail.com>
2026-05-22 14:58:08 +0900

    #401 - [FEAT] ERROR : 호가 관련 비즈니스 에러 추가

--- apps/server-stock/src/main/java/com/assetmind/server_stock/stock/application/OrderBookService.java
+++ apps/server-stock/src/main/java/com/assetmind/server_stock/stock/application/OrderBookService.java
@@ -2,7 +2,8 @@ package com.assetmind.server_stock.stock.application;
 
 import com.assetmind.server_stock.global.error.ErrorCode;
 import com.assetmind.server_stock.market_access.domain.OrderBook;
-import com.assetmind.server_stock.stock.exception.InvalidStockParameterException;
+import com.assetmind.server_stock.stock.application.provider.StockMetadataProvider;
+import com.assetmind.server_stock.stock.exception.InvalidOrderBookParameterException;
 import com.assetmind.server_stock.stock.infrastructure.throttling.OrderBookCacheRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -12,11 +13,17 @@ import org.springframework.stereotype.Service;
 public class OrderBookService {
 
     private final OrderBookCacheRepository cacheRepository;
+    private final StockMetadataProvider stockMetadataProvider;
 
     /**
      * 특정 종목의 최신 호가 스냅샷을 조회
      */
     public OrderBook getOrderBookSnapshot(String stockCode) {
+
+        if(!stockMetadataProvider.isExist(stockCode)) {
+            throw new InvalidOrderBookParameterException(ErrorCode.UNSUPPORTED_ORDER_BOOK);
+        }
+
         return cacheRepository.getSnapshot(stockCode);
     }
 }
